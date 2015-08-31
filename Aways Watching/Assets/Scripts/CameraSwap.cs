ab7c20987c3ca94f72aa7a20846fa0842dffeb0c
miojow <miojow.games@gmail.com>
2015-08-31 20:08:38 -0300

    Adicionado personagem e Sistema de Cameras

---
+++ Aways Watching/Assets/Scripts/CameraSwap.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwap : MonoBehaviour {
+
+    private Camera thisCam;
+    public GameObject player;
+    private Controller Controller;
+
+    void Awake()
+    {
+        thisCam = gameObject.GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 screenPos = thisCam.WorldToScreenPoint(player.transform.position);
+        Ray ray = thisCam.ScreenPointToRay(screenPos);
+        if (Physics.Raycast(ray,out hit)) {
+            Transform objectHit = hit.transform;
+            Debug.Log(hit.collider.name);
+            if (objectHit.tag == "Player")
+            {
+                Debug.Log("ACHOU O PLAYER NA CAMERA: "+thisCam.transform.name);
+                Controller = objectHit.gameObject.GetComponent<Controller>();
+                if ((Controller.camera  == null) || (Controller.camera.transform.position != this.transform.position))
+                {
+                    if (Controller.camera  != null)
+                        Controller.camera.enabled = false;
+                    thisCam.enabled = true;
+                    Controller.camera = thisCam;
+                }
+            }
+         }
+
+        if (thisCam.enabled == true)
+        {
+            thisCam.transform.LookAt(player.transform);
+        }
+
+        Debug.DrawLine(transform.position, hit.point, Color.cyan);
+    }
+
+
+
+
+
+
+
+}
---
+++ Aways Watching/Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+    public CharacterController CharacterController;
+    public Camera camera;
+    public float speed = 3.0f;
+    public float rotateSpeed = 3.0f;
+
+    void Awake()
+    {
+        CharacterController = gameObject.GetComponent("CharacterController") as CharacterController;
+    }
+
+    void Update()
+    {
+        //Rotaciona o Eixo Y
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed,0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        CharacterController.SimpleMove(forward * curSpeed);
+    }
+}
+
+
+
