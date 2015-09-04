fc60380e234f5a76385c6134cff9c63895f40c73
miojow <miojow.games@gmail.com>
2015-09-03 21:55:54 -0300

    Edit de Monstros e NavMesh

--- Aways Watching/Assets/Scripts/Monster.cs
+++ Aways Watching/Assets/Scripts/Monster.cs
@@ -10,15 +10,16 @@ public class Monster : MonoBehaviour {
      public int Life;
      public float Power;
      public float lookDist = 10;
-     public float rotateSpeed = 5;
      private GameObject Player;
      private Controller controller;
      private Rigidbody rigdBody;
      public bool follow = false;
+     private NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
         Player = GameObject.FindGameObjectWithTag("Player");
         rigdBody = gameObject.GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
         rigdBody.isKinematic = true;
         switch(MonsterType)
         {
@@ -29,7 +30,7 @@ public class Monster : MonoBehaviour {
                 Power = 10;
                 break;
             case Type.Passive:
-                Distance = 1f;
+                Distance = 5f;
                 Speed = 0.5f;
                 Life = 50;
                 Power = 30;
@@ -42,33 +43,16 @@ public class Monster : MonoBehaviour {
 
     void Update()
     {
-        if (this.gameObject.name == "Monster")
-        {
-            Debug.Log(Vector3.Distance(transform.position, Player.transform.position));
-        }
+
         if (Vector3.Distance(transform.position, Player.transform.position) <= lookDist)
         {
-            //Quaternion rotation = Quaternion.LookRotation(Player.transform.position - transform.position);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
             transform.LookAt(Player.transform.position);
-            RaycastHit hit;
             //Se o jogador estiver ao alcance
             if (Vector3.Distance(transform.position, Player.transform.position) <= Distance)
             {
                 rigdBody.isKinematic = false;
-                //Joga o Raycast
-                if (Physics.Raycast(transform.position, Player.transform.position, out hit, Distance))
-                {
-                    //Se acertar o Jogador
-                    Transform objectHit = hit.transform;
-                    if (objectHit.tag == "Player")
-                    {
                         follow = true;
-                    }
-                }
-                Debug.DrawLine(transform.position, hit.point, Color.red);
-            }
-
+             }
         }
         else
         {
@@ -79,7 +63,7 @@ public class Monster : MonoBehaviour {
 
         if (follow)
         {
-            transform.position += transform.forward * Speed * Time.deltaTime;
+            agent.SetDestination(Player.transform.position);
         }
 
     }
