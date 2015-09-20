147d21f4adc3b5969ef73cad58fa44e009a7c9a2
miojow <miojow.games@gmail.com>
2015-09-20 19:57:13 -0300

    Edit 20/09/2015 (2)
    Terminado o Item Options;
    Portas agora abrem com chaves específicas;

---
+++ Aways Watching/Assets/Scripts/Cenario/Door.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public int KeyID;
+    string KeyName;
+    TextPanel t;
+    void Awake()
+    {
+         t= GameObject.FindGameObjectWithTag("TextPanel").GetComponent<TextPanel>();
+    }
+
+    void OnTriggerStay(Collider Other)
+    {
+        if (Other.tag == "Player")
+        {
+            if (Other.GetComponent<Controller>().Key == KeyID)
+            {
+                
+                ItemDatabase database = GameObject.FindGameObjectWithTag("ItemDatabase").GetComponent<ItemDatabase>();
+
+                foreach (Item i in database.items){
+                    if(i.itemID == KeyID)
+                    {
+                        KeyName = i.itemName;
+                        break;
+                    }
+                }
+                string Message = "Você abriu a porta com a chave "+KeyName;
+                t.WriteText(Message);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+
+}
